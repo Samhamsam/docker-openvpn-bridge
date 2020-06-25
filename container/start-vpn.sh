78620ce43fa543976cf51bcdd6c16b6bcc289e5b
Mark Unger <mark.unger@criptext.com>
2020-06-25 13:52:07 +0000

    init

---
+++ container/start-vpn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Needed for openvpn
+mkdir -p /run/openvpn
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+
+ip link del dev tap0 ;
+
+echo "create dev tap0 with openvpn --mktun"
+openvpn --mktun --dev tap0 ;
+
+echo "tap0 promisc on up"
+ip link set tap0 promisc on up ;
+
+echo "eno1 promisc on up"
+ip link set eno1 promisc on up ;
+
+echo "add tap0 to br0"
+brctl addif br0 tap0 ;
+
+
+chown -R nobody:nogroup /etc/openvpn
+chmod -R 700 /etc/openvpn
+
+echo "enable forwarding"
+sysctl -w net.ipv4.ip_forward=1
+
+# Run actual OpenVPN
+exec /usr/sbin/openvpn --writepid /run/openvpn/server.pid --cd /etc/openvpn --config /etc/openvpn/server.conf --script-security 2
